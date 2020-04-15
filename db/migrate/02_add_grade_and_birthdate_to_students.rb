aa9d30dc4c03882e1039d3f296c1ab1a2c30df61
fadimata tillman <fadi.tillman@gmail.com>
2020-04-15 19:08:52 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
   def change
     add_column   :students do |t|
-      t.integer :grade
-      t.tring :birthday
+      t.integer  :grade
+      t.tring    :birthday
      end
    end
